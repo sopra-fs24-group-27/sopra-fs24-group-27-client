355c6bd5f5b4007b51d436630c4245e911b99a43
YatingPan <panyating0322@gmail.com>
2024-04-26 19:36:31 +0200

    try to solve test after deploy

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -8,7 +8,7 @@ import { isProduction } from "./isProduction"
  */
 
 export const getDomain = () => {
-  const prodUrl = "https://sopra-fs24-group-27-server.oa.r.appspot.com";
+  const prodUrl = "https://sopra-fs24-group-27-server.oa.r.appspot.com"; 
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 };

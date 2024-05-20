277d298df1b6c174c0cade07564e3135cbbbe008
YatingPan <panyating0322@gmail.com>
2024-05-20 13:45:09 +0200

    recopy and cover register

--- src/components/views/Register.tsx
+++ src/components/views/Register.tsx
@@ -1,17 +1,17 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import NightlifeOutlinedIcon from "@mui/icons-material/NightlifeOutlined";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import NightlifeOutlinedIcon from '@mui/icons-material/NightlifeOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState } from 'react';
 import { api, handleError } from "helpers/api";
 import User from "models/User";
 import { useNavigate } from "react-router-dom";
@@ -26,14 +26,15 @@ import { useNavigate } from "react-router-dom";
 
 // const avatarComponents = [AvatarSvg1, AvatarSvg2, AvatarSvg3, AvatarSvg4, AvatarSvg5, AvatarSvg6, AvatarSvg7];
 
+
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: "#7e57c2",
+      main: '#7e57c2',
     },
   },
   typography: {
-    fontFamily: "Comic Sans MS",
+    fontFamily: 'Comic Sans MS',
   },
 });
 
@@ -64,8 +65,8 @@ export default function Register() {
         // localStorage.setItem('username', user.username);
         // localStorage.setItem('currentUserId', user.id);
         sessionStorage.setItem("token", user.token);
-        sessionStorage.setItem("userId", user.id);
-        sessionStorage.setItem("username", user.username);
+        sessionStorage.setItem('userId', user.id);
+        sessionStorage.setItem('username', user.username);
         // sessionStorage.setItem("currentUserId", user.id);
 
         // Navigate to user overview page with necessary information
@@ -74,46 +75,24 @@ export default function Register() {
         throw new Error("Registration failed");
       }
     } catch (error) {
-      alert("The user name already exists. Try another one :D");
+      alert(`The user name already exists. Try another one :D`);
     }
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        style={{ minHeight: "100vh", marginTop: "-80px" }}
-      >
+      <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh', marginTop: '-80px' }}>
         <Grid item xs={12} sm={8} md={4}>
-          <Paper
-            elevation={6}
-            square
-            sx={{
-              backgroundColor: "rgba(235, 200, 255, 0.7)",
-              borderRadius: "50px 10px 50px 10px",
-            }}
-          >
-            <Box
-              p={4}
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-            >
-              <Avatar sx={{ m: 1, bgcolor: "#ba68c8" }}>
+          <Paper elevation={6} square sx={{ backgroundColor: 'rgba(235, 200, 255, 0.7)', borderRadius: '50px 10px 50px 10px' }}>
+            <Box p={4} display="flex" flexDirection="column" alignItems="center">
+              <Avatar sx={{ m: 1, bgcolor: '#ba68c8' }}>
                 <NightlifeOutlinedIcon />
               </Avatar>
               <Typography component="h1" variant="h5">
                 Register
               </Typography>
-              <Box
-                component="form"
-                noValidate
-                onSubmit={handleRegister}
-                sx={{ mt: 1 }}
-              >
+              <Box component="form" noValidate onSubmit={handleRegister} sx={{ mt: 1 }}>
                 <TextField
                   margin="normal"
                   required
